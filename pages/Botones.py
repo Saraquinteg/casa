2c2e98817d267c72066bf5b324163bca24a74278
Saraquinteg <84721198+Saraquinteg@users.noreply.github.com>
2023-11-16 18:08:24 -0500

    Update Botones.py

--- pages/Botones.py
+++ pages/Botones.py
@@ -29,17 +29,17 @@ st.title(":violet[CASA INTELIGENTE DE LA BARBIE]")
 st.write("Bienvenida a la Casa Inteligente de Barbie, donde la moda se encuentra con la tecnología en un entorno lleno de estilo"
 " y comodidades modernas. Esta casa vanguardista redefine la experiencia de jugar con Barbie, ofreciendo un hogar totalmente conectado y equipado con las últimas innovaciones.")
 
-image = Image.open('voz.png')
+image = Image.open('boton.png')
 st.image('voz.png', width=70)
 
-st.subheader(":violet[PASOS PARA ABRIR LA PUERTA]")
-st.write("La entrada principal de la Casa Inteligente de Barbie es una puerta vanguardista que combina elegancia con tecnología de última generación."
-" Esta puerta inteligente redefine la experiencia de acceso al hogar, proporcionando a Barbie un acceso fácil y seguro con tan solo un comando de voz.")
+st.subheader(":violet[PASOS PARA ENCEDER LA LUZ]")
+st.write("
+¡Presentamos la increíble Luz Inteligente para la Casa de Barbie, la adición perfecta para iluminar y realzar la experiencia en el hogar de la muñeca más famosa del mundo."
+"Esta innovadora luz combina la magia del diseño y la tecnología para brindar un toque moderno y elegante a cada rincón de la casa de Barbie.")
 
-st.write(":violet[Paso 1] Presiona el botón que está aquí abajo para encender el micrófono y pronunciar el comando adecuado.")
-st.write(":violet[Paso 2] Simplemente pronuncia el comando personalizado: ¡Abrir! y la puerta se desliza suavemente hacia un lado, revelando el mundo lujoso y lleno de comodidades que aguarda en el interior.")
-st.write(":violet[Paso 3] Finalmente con comando personalizado: ¡Cerrar!, reversarás la acción .")
-st.title(":green[¡Y LISTO!]")
+st.write(":violet[Paso 1] Presiona el botón que está aquí "ON" para encender la luz.")
+st.write(":violet[Paso 1] Presiona el botón que está aquí "Off" para apagar la luz.")
+st.title(":orange[¡Y LISTO!]")
 
 
 if st.button('ON'):
